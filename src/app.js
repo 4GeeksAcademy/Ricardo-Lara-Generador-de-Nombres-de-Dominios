0adc70dbec0adff5e56f80d7bfd332db335a9463
rlarag12 <rlarag1297@gmail.com>
2025-12-16 03:05:33 +0000

    casi casi

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let dom = ['.com', '.net'];
+  let dominiosGenerados = '';
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let p = 0; p < dom.length; p++) {
+          let dominios = pronoun[i] + adj[j] + noun[k] + dom[p];
+          console.log(dominios);
+         
+           dominiosGenerados += dominio + '<br>';
+        }
+      }
+    }
+  }
+
+  document.getElementById('dominio').innerHTML = dominiosGenerados;
+  
 };
